8ebdc967f655591ed86efb772c7c92dae07ec19b
Waseem idrisi <waseem@greenlava.in>
2023-01-16 21:18:06 +0530

    java notes

---
+++ Sum.java
@@ -0,0 +1,15 @@
+/**
+ * Sum
+ */
+public class Sum {
+
+    public static void main(String[] args) {
+        
+         int a = 2;
+         int b = 6;
+         int c = a+b;
+
+         System.out.println("sum is "+c);
+    }
+    
+}
