0c668ed78b1ad68994d36994b2efed6a8e6374ac
Waseem idrisi <waseem@greenlava.in>
2023-01-28 21:22:36 +0530

    BasicPatterns

--- Loops/BasicPatterns.java
+++ Loops/BasicPatterns.java
@@ -83,6 +83,78 @@ public class BasicPatterns {
         }
 */
 
+       /* Q.6) print the pattern
+         1
+         12
+         123
+         1234
+         12345
+
+        int n = 5 ;
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }*/
+
+      /* Q.7 print the pattern
+         12345
+         1234
+         123
+         12
+         1
+        int n = 5;
+        for(int i=n ; i>=1 ; i--){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }*/
+
+
+/*
+        Q.8 print the pattern
+         1
+         2 3
+         4 5 6
+         7 8 9 10
+         11 12 13 14 15
+
+        int n= 5;
+        int k=1;
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println("");
+        }
+*/
+
+
+//        Q.9 print the pattern
+//         1
+//         01
+//         101
+//         0101
+//         10101
+        int n = 5;
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1; j<=i ; j++){
+                int sum = i+j;
+                if(sum%2 == 0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+
+
+
         //Break
         /*Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
